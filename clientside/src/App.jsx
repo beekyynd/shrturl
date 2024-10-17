2e6052cb404ff3923bf0f8d9cbb9a4bb27482ec8
Francis Kalu <snapchainx@gmail.com>
2024-10-18 00:54:07 +0100

    fixed import issue

--- clientside/src/App.jsx
+++ clientside/src/App.jsx
@@ -2,9 +2,6 @@ import { Box } from "@chakra-ui/react";
 
 import {Route, Routes} from "react-router-dom";
 
-
-import CreatePage from "./pages/CreatePage";
-
 import HomePage from "./pages/HomePage";
 
 import Navbar from "./components/Navbar";
